5cfe5cc45713518fc9c0a2869f96dd096ffc96fc
mtk30765 <wei.chenglee@mediatek.com>
2025-03-11 13:34:50 +0800

    feat: 添加 AnalogAgentMode 和 AnalogAgentArgs 類別以支持多種代理模式和參數配置

--- analog_agent.py
+++ analog_agent.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Any, Literal, Optional
 from pathlib import Path
 import datetime
@@ -232,6 +233,104 @@ class CodeBlock(BaseModel):
     @property
     def code_in_markdown(self) -> str:
         return f"```{self.code_type}\n{self.code_content}\n```"
+
+
+class AnalogAgentMode(str, Enum):
+    # Using ChatCompletion
+    original: str = "original"
+    # Using GroupChat Based
+    groupchat: str = "groupchat"
+    # Using Captain Agent
+    captain: str = "captain"
+    # Using Swarm Agent
+    swarm: str = "swarm"
+    # TestBench Mode
+    groupchat_tba: str = "groupchat+tba"
+    # RAG Mode
+    groupchat_rag: str = "groupchat+rag"
+    captain_rag: str = "captain+rag"
+
+
+class AnalogAgentArgs(BaseModel):
+    model: str = Field(
+        ...,
+        title="The Model Name",
+        description="The model name you want to use, such as aide-gpt-4o.",
+        examples=["aide-gpt-4o", "aide-o3-mini"],
+        frozen=True,
+        deprecated=False,
+    )
+    messages: list[dict[str, str]] = Field(
+        ...,
+        title="The Original Input Messages",
+        description="The original input messages to the agent; it should follow the format of openai ChatCompletion.",
+        frozen=True,
+        deprecated=False,
+    )
+    mode: AnalogAgentMode = Field(
+        ...,
+        title="The Mode of the Agent",
+        description="The mode of the agent, such as original, captain, groupchat, etc.",
+        examples=[
+            "original",
+            "groupchat",
+            "captain",
+            "swarm",
+            "groupchat+tba",
+            "captain+rag",
+            "groupchat+rag",
+        ],
+        frozen=True,
+        deprecated=False,
+    )
+    work_dir: str = Field(
+        default=".",
+        title="The Working Directory",
+        description="The working directory groupchat can use; it is not required if you are using `original` mode.",
+        examples=[".", "./path/to/folder"],
+        frozen=False,
+        deprecated=False,
+    )
+    groupchat_config: str = Field(
+        default="./configs/agents/groupchat_wo_cos.yaml",
+        title="The GroupChat Config File",
+        description="The groupchat config file you want to use; it is not required if you are using `original` mode.",
+        examples=["./configs/agents/groupchat.yaml", "./configs/agents/groupchat_wo_cos.yaml"],
+        frozen=False,
+        deprecated=False,
+    )
+    task: str = Field(
+        default="",
+        title="The Task Name",
+        description="This is the Circuit name from problem set.",
+        examples=["a single-stage common-source amplifier with resistive load R"],
+        frozen=False,
+        deprecated=False,
+    )
+    task_type: str = Field(
+        default="",
+        title="The Task Type",
+        description="This is the Circuit type from problem set, it is required only if you are using `groupchat+tba` mode.",
+        examples=["Amplifier", "Opamp", "CurrentMirror", "Inverter"],
+        frozen=False,
+        deprecated=False,
+    )
+    input_nodes: str = Field(
+        default="",
+        title="The Input Nodes",
+        description="This is the input nodes of the circuit, it is required only if you are using `groupchat+tba` mode.",
+        examples=["Vin", "Vbias"],
+        frozen=False,
+        deprecated=False,
+    )
+    output_nodes: str = Field(
+        default="",
+        title="The Output Nodes",
+        description="This is the output nodes of the circuit, it is required only if you are using `groupchat+tba` mode.",
+        examples=["Vout", "Vbias"],
+        frozen=False,
+        deprecated=False,
+    )
 
 
 class AnalogAgent(BaseModel):
